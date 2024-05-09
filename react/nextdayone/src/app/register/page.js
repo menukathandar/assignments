f46b27afa71efaf6ec7b58d1e388f25d30d00f92
unknown <menukathandar@gmail.com>
2024-05-09 13:55:47 +0545

    Register page validated

--- react/nextdayone/src/app/register/page.js
+++ react/nextdayone/src/app/register/page.js
@@ -1,26 +1,156 @@
-// import NavBar from '../components/navbar/page';
-import React from 'react'
-import {Button,Input} from "@nextui-org/react";
+'use client'
 
-const page = () => {
+import React from 'react';
+import { useFormik } from 'formik';
+import {Button,Input} from "@nextui-org/react";
+import * as Yup from 'yup';
+ 
+const SignupSchema = Yup.object().shape({
+  Fullname: Yup.string()
+    .min(5, 'Too Short!')
+    .max(25, 'Too Long!')
+    .required('Required'),
+  Address: Yup.string()
+    .min(5, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  DateOfBirth: Yup.string()
+    .required('Required'), 
+  Email: Yup.string().email('Invalid email').required('Required'),
+  Username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'), 
+  Password: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'), 
+  
+});
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+     Fullname: '',
+     Address: '',
+     DateOfBirth: '',
+     Email: '',
+     Username:'',
+     Password:''
+    },
+    validationSchema:SignupSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <div>
-      {/* <NavBar/> */}
-        <div className="flex justify-center">
-          <div className= 'border border-black m-7 w-72 p-2'>
-          <Input type="text" label="Fullname" />
-          <Input type="text" label="Address" />
-          <Input type="date" label="Date of birth" />
-          <Input type="text" label="Username" />
-          <Input type="Password" label="Password" />
-          <Input type="email" label="Email" />
-          <Button radius="full" className="bg-gradient-to-tr from-green-500 to-blue-500 text-white shadow-lg">
-            Sign Up
-          </Button>
+    <form onSubmit={formik.handleSubmit}>
+       <div>
+          <div className="flex justify-center">
+           <div className= 'border border-black m-7 w-72 p-2'>
+           <div className="mb-2">
+            <label htmlFor="Fullname"></label>
+              <Input
+                isClearable
+                label="Fullname"
+                variant="bordered"
+                onChange={formik.handleChange}
+                name="Fullname"
+                value={formik.values.Fullname}
+                placeholder="Enter your Fullname"
+                className="max-w-xs"
+              />
+              {formik.errors.Fullname}
+            </div>
+            <div className="mb-2">
+            <label htmlFor="Address"></label>
+              <Input
+                isClearable
+                label="Address"
+                variant="bordered"
+                onChange={formik.handleChange}
+                name="Address"
+                value={formik.values.Address}
+                placeholder="Enter your Address"
+                className="max-w-xs"
+              />
+              {formik.errors.Address}
+            </div>
+            <div className="mb-2">
+            <label htmlFor="DateOfBirth"></label>
+              <Input
+                isClearable
+                type="date"
+                label="DateOfBirth"
+                variant="bordered"
+                onChange={formik.handleChange}
+                name="DateOfBirth"
+                value={formik.values.DateOfBirth}
+                placeholder="Enter your DateOfBirth"
+                defaultValue="junior@nextui.org"
+                onClear={() => console.log("input cleared")}
+                className="max-w-xs"
+              />
+              {formik.errors.DateOfBirth}
+            </div>
+            <div className="mb-2">
+            <label htmlFor="Email"></label>
+              <Input
+                isClearable
+                type="Email"
+                label="Email"
+                variant="bordered"
+                onChange={formik.handleChange}
+                name="Email"
+                value={formik.values.Email}
+                placeholder="Enter your Email"
+                defaultValue="junior@nextui.org"
+                onClear={() => console.log("input cleared")}
+                className="max-w-xs"
+              />
+              {formik.errors.Email}
+            </div>
+            <div className="mb-2">
+            <label htmlFor="Username"></label>
+              <Input
+                isClearable
+                label="Username"
+                variant="bordered"
+                onChange={formik.handleChange}
+                name="Username"
+                value={formik.values.Username}
+                placeholder="Enter your Username"
+                defaultValue="junior@nextui.org"
+                onClear={() => console.log("input cleared")}
+                className="max-w-xs"
+              />
+              {formik.errors.Username}
+            </div>
+            <div className="mb-2">
+            <label htmlFor="Password"></label>
+              <Input
+                isClearable
+                type="Password"
+                label="Password"
+                variant="bordered"
+                onChange={formik.handleChange}
+                name="Password"
+                value={formik.values.Password}
+                placeholder="Enter your Password"
+                onClear={() => console.log("input cleared")}
+                className="max-w-xs"
+              />
+              {formik.errors.Password}
+            </div>
+              <Button type="submit" radius="full" className="bg-gradient-to-tr from-green-500 to-blue-500 text-white shadow-lg">
+                Sign Up
+              </Button>
           </div>
         </div>
-    </div>
-  )
-}
+      </div>
+    </form>
+  );
+};
 
-export default page
+
+export default SignupForm
