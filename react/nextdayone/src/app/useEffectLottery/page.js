5c99bec0b606e40bcba7154f9f6ed6daf47ae6b2
unknown <menukathandar@gmail.com>
2024-05-10 17:08:11 +0545

    Basic Lottery page created with pause

--- react/nextdayone/src/app/useEffectLottery/page.js
+++ react/nextdayone/src/app/useEffectLottery/page.js
@@ -15,21 +15,36 @@ const page = () => {
         'X'
     ]
     const [lotteryId, setLotteryId] = useState(null)
+    const [isPaused, setIsPaused] = useState(false)
     useEffect(()=>{
-        setInterval(() => {
-            const randomNum = Math.ceil(Math.random()*10)
-            setLotteryId(randomNum)
-        }, 100);
-    },[])
+        if(!isPaused){
+            setTimeout(()=>{
+                let randomNum = Math.ceil(Math.random()*lotteryItems.length)
+                if(randomNum === lotteryId){
+                    randomNum = randomNum+1
+                }
+                setLotteryId(randomNum)
+            }, 100);
+        }
+    },[lotteryId, isPaused])
+    const pauseGame = () =>{
+        setIsPaused(!isPaused)
+    }
+    const generateResult = ()=>{
+        if(lotteryItems[lotteryId] !== 'X' && isPaused){
+            return <div>Congratulations!!You won</div>
+        }else if(lotteryItems[lotteryId] == 'X' && isPaused)
+            return <div>You Lost</div>
+    }
   return (
     <div>
-        {lotteryId}
         {
             lotteryItems.map((item, id)=>{
-                return <div style={{backgroundColor: id===lotteryId ? 'red' : null}} className='w-32 shadow-lg m-2 p-2'>{item}</div>
+                return <div key = {id} style={{backgroundColor: id===lotteryId ? 'red' : null}} className='w-32 shadow-lg m-2 p-2'>{item}</div>
             })
         }
-        <button>Pause</button>
+        {generateResult()}
+        <button onClick={pauseGame}>{isPaused ? 'start' : 'Pause'}</button>
     </div>
   )
 }
